fa5f6198d42a6ef66cdde0f90518734bff2fe904
nsharandroidnstudio <85453900+nsharandroidnstudio@users.noreply.github.com>
2023-12-06 18:36:35 +0200

    Update main,classifier
    The update include a use of the trained model, faster results.

--- Classifier/classifier.py
+++ Classifier/classifier.py
@@ -1,76 +1,30 @@
-from gpt4all import GPT4All
+import torch
+from setfit import SetFitModel
 
-class AntiIsraelClassifier:
-    """
-    A class for classifying sentences as anti-Israeli or pro-Israeli using GPT-4.
+class AntiIsraelClassifierModelSetFit:
+        
+        def __init__(self, model_path= "SavedModel\\model.joblib"):
 
-    Args:
-        model_path (str): The path to the GPT-4 model.
-    """
+            self.model = SetFitModel._from_pretrained(model_path)
 
-    def __init__(self, model_path= "mistral-7b-instruct-v0.1.Q4_0.gguf",temp=0.5):
-        self.model = GPT4All(model_path)
-        self.temp = temp
-
-    def classify_Is_it_anti_pro_isreali(self, sentence: str) -> bool:
-        """
-        Classifies the given sentence as anti-Israeli or pro-Israeli.
-
-        Args:
-            sentence (str): The sentence to be classified.
-
-        Returns:
-            bool: True if the sentence is classified as anti-Israeli, False otherwise.
-        """
-        input_prompt = (
-    "Task:Sentiment classification Classes:pro israeli,anti Israeli\n"
-    "Context:Classify text as pro or anti-Israeli based on tone,context,and language cues.Adhere strictly to explicit content,avoiding assumptions or interpretations.Maintain neutrality,provide factual analysis,and refrain from offering opinions,political insights,or historical context. In ambiguous cases,note uncertainty make an arbitrary classification."
-    "Examples:\n"
-    "Text:From the river to the sea, Palestine will be free.\n"
-    "Label:anti Israeli\n"
-    "Text:I stand with isreal\n"
-    "Label:pro isreali\n"
-    "Text:hamas=isis\n"
-    "Label:pro israeli"
-    "Analyze sentiment in the text.\n"
-    f"text:'{sentence}'\n"
-)
-
-        output = self.model.generate(input_prompt, max_tokens=14, temp=self.temp)
-        return "anti" in output.lower()
-
-    
-    def Scoring_Scale_anti_pro_isreali(self, sentence: str) -> str:
-            
+        def classify_Is_it_anti_pro_isreali(self, sentence: str) -> bool:
             """
-            Assigns a sentiment score on the anti-Israel to pro-Israel scale for the given sentence.
+            Classifies the given sentence as anti-Israeli or pro-Israeli.
 
             Args:
-            sentence (str): The input sentence for sentiment analysis.
+            sentence (str): The sentence to be classified.
 
-             Returns:
-            float: A sentiment score on the scale of -1 (indicating anti-Israel sentiment)to 1(indicating pro-Israel sentiment).
+            Returns:
+            bool: True if the sentence is classified as anti-Israeli, False otherwise.
             """
-            input_prompt = (
-    "Task:Sentiment scoring\n"
-    "Context:Score a sentence's sentiment on a scale from -1(anti-Israel)to 1(pro-Israel)considering tone,context,and language cues.\n"
-    "Examples:\n"
-    "Text:I Stand with Israel.\n"
-    "Score:1\n"
-    "Text:hamas=isis\n"
-    "Score:0.9\n"
-    "Text:stop antisemitism\n"
-    "Score:0.8\n"
-    "Israel is doing a genocide to the Palestinian\n"
-    "Score:-1"
-    "Text:Stop isreal occupation they making a PROPAGANDA\n"
-    "Score:-0.8\n"
-    "Text:From the river to the sea Palestinian will be free\n"
-    "Score:-1\n"
-    "give a score for the text.\n"
-    f"text:{sentence}\n"
-)
-   
+            import numpy as np
+            prediction = self.model.predict([sentence])
+            probabilities = self.model.predict_proba([sentence])
+            probabilities_index = torch.argmax(probabilities)
+            predicted_class_probability = probabilities[0, probabilities_index]
+           
+            if int(prediction) == 0:
+                 return True,predicted_class_probability.item()
+            return False,predicted_class_probability.item()
 
-            output = self.model.generate(input_prompt, max_tokens=14, temp=self.temp)
-            return output
+    
--- Classifier/main.py
+++ Classifier/main.py
@@ -1,11 +1,10 @@
 from typing import List
-from classifier import AntiIsraelClassifier
+from classifier import AntiIsraelClassifierModelSetFit
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
 app = FastAPI()
-classifier = AntiIsraelClassifier()
-
+classifierSetfit = AntiIsraelClassifierModelSetFit()
 class One_Sentence(BaseModel):
     sentence: str
 
@@ -31,9 +30,12 @@ def classify_sentence(one_sentence: One_Sentence):
             raise HTTPException(status_code=400, detail="Sentence parameter is required.")
 
         text_data = one_sentence.sentence
-        is_anti_israel = classifier.classify_Is_it_anti_pro_isreali(text_data)
-        return {"sentence": text_data, "Is it anti-Israeli": is_anti_israel}
-
+        is_anti_israel,probability = classifierSetfit.classify_Is_it_anti_pro_isreali(text_data)
+        return {
+        "sentence": text_data,
+        "is_anti_israel": is_anti_israel,
+        "probability": probability
+    }
     except Exception as e:
         return {"error": str(e)}
 
@@ -56,62 +58,15 @@ def classify_multiple_sentences(Multiple_Sentence: Multiple_Sentnces):
         sentences = Multiple_Sentence.list_of_sentences
         classification_responses = []
         for sentence in sentences:
-            is_anti_israel = classifier.classify_Is_it_anti_pro_isreali(sentence)
-            classification_responses.append({"Sentence": sentence, "Is it anti-Israeli": is_anti_israel})
+            is_anti_israel,probability = classifierSetfit.classify_Is_it_anti_pro_isreali(sentence)
+            classification_responses.append({
+            "Sentence": sentence,
+            "Is it anti-Israeli": is_anti_israel,
+            "Probability": probability
+            })
 
         return classification_responses
 
     except Exception as e:
         return {"error": str(e)}
 
-@app.post("/Scoring_Anti_pro_isreali/MultipleSentences", response_model=list[dict])
-def Scoring_multiple_sentences(Multiple_Sentence: Multiple_Sentnces):
-    """
-    Score multiple sentences.
-
-    Args:
-        multiple_sentences (Multiple_Sentences): Request model containing a list of sentences to be scored.
-        content_type (str): Header parameter.
-
-    Returns:
-        dict: JSON response containing the scoring results.
-    """
-    try:
-        if not Multiple_Sentence.list_of_sentences:
-            raise HTTPException(status_code=400, detail="Sentence parameter is required and must be a list of sentences.")
-
-        sentences = Multiple_Sentence.list_of_sentences
-        classification_responses = []
-        for sentence in sentences:
-            score = classifier.Scoring_Scale_anti_pro_isreali(sentence)
-            key, score = score.split(':')
-
-            classification_responses.append({"Sentence": sentence, "score": score})
-
-        return classification_responses
-
-    except Exception as e:
-        return {"error": str(e)}
-
-@app.post("/Scoring_Anti_pro_isreali/OneSentence", response_model=dict)
-def classify_sentence(one_sentence: One_Sentence):
-    """
-    Classify the sentiment of the given sentence.
-
-    Args:
-        one_sentence (One_Sentence): Request model containing the sentence to be classified.
-
-    Returns:
-        dict: JSON response containing the scoring results.
-    """
-    try:
-        if one_sentence.sentence is None:
-            raise HTTPException(status_code=400, detail="Sentence parameter is required.")
-
-        text_data = one_sentence.sentence
-        score = classifier.Scoring_Scale_anti_pro_isreali(text_data)
-        key, score = score.split(':')
-        return {"sentence": text_data, "score": score}
-
-    except Exception as e:
-        return {"error": str(e)}
